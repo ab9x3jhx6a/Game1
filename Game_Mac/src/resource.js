3bb435b03919a7e87e0f5ac121b9214432727a42
Stephan Jiang <stephen665021@gmail.com>
2015-09-23 20:40:52 -0400

    Include Game_Mac for reference

---
+++ Game_Mac/src/resource.js
@@ -0,0 +1,54 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    MainScene_json : "res/MainScene.json",
+    background_png : "res/background.png",
+    paperFalling_png : "res/paperFalling.png",
+    desk_png : "res/desk.png",
+    frame_png : "res/frame.png",
+    rest_png : "res/rest.png",
+    right0_png : "res/right0.png",
+    left0_png : "res/left0.png",
+    left_png  : "res/left.png",
+    left_plist : "res/left.plist",
+    right_png : "res/right.png",
+    right_plist : "res/right.plist",
+
+    bni1_png : "res/bni1.png",
+
+    BossNormIdle_png : "res/BossNormIdle.png",
+    BossNormIdle_plist : "res/BossNormIdle.plist",
+    BossNormThrow_png : "res/BossNormThrow.png",
+    BossNormThrow_plist : "res/BossNormThrow.plist",
+
+    BossAng1Idle_png : "res/BossAng1Idle.png",
+    BossAng1Idle_plist : "res/BossAng1Idle.plist",
+    BossAng1Throw_png : "res/BossAng1Throw.png",
+    BossAng1Throw_plist : "res/BossAng1Throw.plist",
+
+    BossAng2Idle_png : "res/BossAng2Idle.png",
+    BossAng2Idle_plist : "res/BossAng2Idle.plist",
+    BossAng2Throw_png : "res/BossAng2Throw.png",
+    BossAng2Throw_plist : "res/BossAng2Throw.plist",
+    
+    BossAng3Idle_png : "res/BossAng3Idle.png",
+    BossAng3Idle_plist : "res/BossAng3Idle.plist",
+    BossAng3Throw_png : "res/BossAng3Throw.png",
+    BossAng3Throw_plist : "res/BossAng3Throw.plist",
+
+    scene1 : "res/sound/scene1.ogg",
+    paper_catch : "res/sound/paper_catch.wav",
+    paper1 : "res/sound/paper1.wav",
+    fail : "res/sound/fail.mp3",
+    victory : "res/sound/victory.mp3",
+    boss_anger : "res/sound/boss_anger.wav",
+
+    winscreen_png : "res/winscreen.png",
+    failscreen_png : "res/failscreen.png"
+    
+};
+
+var g_resources = [];
+
+for (var i in res) {
+    g_resources.push(res[i]);
+}
